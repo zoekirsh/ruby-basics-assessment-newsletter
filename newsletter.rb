8c8bfc7864ef593b2603807d759c70d4849a0f70
Rob Cobb <rwcobbjr@gmail.com>
2019-06-14 17:28:38 -0400

    test solution with sections

--- newsletter.rb
+++ newsletter.rb
@@ -20,17 +20,17 @@ SUBSCRIBERS = ['rhona@grimes.info', 'cedricschmidt@robel.io', 'edmond@ko.org', '
 UNSUBSCRIBED = ['cedricschmidt@robel.io', 'alverta@bernhard.name', 'julianpadberg@mosciski.com', 'bo@rippin.net', 'gaylebogan@considine.net']
 
 ARTICLES = [
-  { "author": 'Destiny Blanda Bruen II', "title": 'Mining', "text": 'The orthogonal features, when combined, can explode into complexity.' },
-  { "author": 'Robin Flatley Hilpert', "title": 'Retail', "text": 'In our daily lives as programmers, we process text strings a lot. So I tried to work hard on text processing, namely the string class and regular expressions. Regular expressions are built into the language and are very tuned up for use.' },
-  { "author": 'Olevia Torphy Kuvalis', "title": 'Technology', "text": 'Imagine you are writing an email. You are in front of the computer. You are operating the computer, clicking a mouse and typing on a keyboard, but the message will be sent to a human over the internet. So you are working before the computer, but with a human behind the computer.' },
-  { "author": 'Dr. Crystle Kovacek Denesik', "title": 'Legal', "text": 'Most programs are not write-once. They are reworked and rewritten again and again in their lived. Bugs must be debugged. Changing requirements and the need for increased functionality mean the program itself may be modified on an ongoing basis. During this process, human beings must be able to read and understand the original code. It is therefore more important by far for humans to be able to understand the program than it is for the computer.' },
-  { "author": 'Alfred Jast Hermann', "title": 'Real-Estate', "text": "I didn't work hard to make Ruby perfect for everyone, because you feel differently from me. No language can be perfect for everyone. I tried to make Ruby perfect for me, but maybe it's not perfect for you. The perfect language for Guido van Rossum is probably Python." },
-  { "author": 'Michale Bruen Boehm', "title": 'Consulting', "text": "Everyone has an individual background. Someone may come from Python, someone else may come from Perl, and they may be surprised by different aspects of the language. Then they come up to me and say, 'I was surprised by this feature of the language, so therefore Ruby violates the principle of least surprise.' Wait. Wait. The principle of least surprise is not for you only." },
-  { "author": 'Tony Keeling Cartwright', "title": 'Design', "text": 'Often people, especially computer engineers, focus on the machines. But in fact we need to focus on humans, on how humans care about doing programming or operating the application of the machines.' }
+  { "section": "What's Happening On Campus", "author": 'Destiny Blanda Bruen II', "title": 'Mining', "text": 'The orthogonal features, when combined, can explode into complexity.' },
+  { "section": "Student Blogs", "author": 'Robin Flatley Hilpert', "title": 'Retail', "text": 'In our daily lives as programmers, we process text strings a lot. So I tried to work hard on text processing, namely the string class and regular expressions. Regular expressions are built into the language and are very tuned up for use.' },
+  { "section": "What's Happening On Campus", "author": 'Olevia Torphy Kuvalis', "title": 'Technology', "text": 'Imagine you are writing an email. You are in front of the computer. You are operating the computer, clicking a mouse and typing on a keyboard, but the message will be sent to a human over the internet. So you are working before the computer, but with a human behind the computer.' },
+  { "section": "Off Campus Events", "author": 'Dr. Crystle Kovacek Denesik', "title": 'Legal', "text": 'Most programs are not write-once. They are reworked and rewritten again and again in their lived. Bugs must be debugged. Changing requirements and the need for increased functionality mean the program itself may be modified on an ongoing basis. During this process, human beings must be able to read and understand the original code. It is therefore more important by far for humans to be able to understand the program than it is for the computer.' },
+  { "section": "Recent Articles and News", "author": 'Alfred Jast Hermann', "title": 'Real-Estate', "text": "I didn't work hard to make Ruby perfect for everyone, because you feel differently from me. No language can be perfect for everyone. I tried to make Ruby perfect for me, but maybe it's not perfect for you. The perfect language for Guido van Rossum is probably Python." },
+  { "section": "Student Blogs", "author": 'Michale Bruen Boehm', "title": 'Consulting', "text": "Everyone has an individual background. Someone may come from Python, someone else may come from Perl, and they may be surprised by different aspects of the language. Then they come up to me and say, 'I was surprised by this feature of the language, so therefore Ruby violates the principle of least surprise.' Wait. Wait. The principle of least surprise is not for you only." },
+  { "section": "What's Happening On Campus", "author": 'Tony Keeling Cartwright', "title": 'Design', "text": 'Often people, especially computer engineers, focus on the machines. But in fact we need to focus on humans, on how humans care about doing programming or operating the application of the machines.' }
 ]
 
 #########################
-# helpers to print parts of the newsletter
+# helpers for some calculations
 #########################
 
 def calculate_recipients
@@ -38,6 +38,18 @@ def calculate_recipients
       UNSUBSCRIBED.include?(email)
     end
 end  
+
+def calculate_sections
+  ARTICLES.map { |article| article[:section] }.uniq
+end
+
+def articles_by_section
+  ARTICLES.group_by { |a| a[:section] }
+end
+
+#########################
+# helpers to print parts of the newsletter
+#########################
 
 def print_recipients
   puts calculate_recipients.join(", ")
@@ -47,14 +59,26 @@ def print_subject
   puts "#{format_campus_location(CAMPUS)} Newsletter - #{format_week}"
 end
 
+def print_in_todays_newsletter
+  puts "\nIn today's newsletter: \n- #{format_sections(calculate_sections)}\n\n"
+end
+
+def print_section(section, articles)
+    puts section
+    articles.each do |article|
+      puts format_article(article)
+    end
+end
+
 def print_articles
-  ARTICLES.each do |article|
-    puts format_article(article)
+  articles_by_section.each do |section|
+    print_section(*section)
   end
 end
 
 def print_newsletter
   puts "#{format_campus_location(CAMPUS)} Newsletter - #{format_week}"
+  print_in_todays_newsletter
   print_articles
   puts format_footer(CAMPUS)
 end
@@ -71,6 +95,10 @@ def format_week
   DATE.strftime '%b %d, %Y'
 end
 
+def format_sections(sections)
+  sections.join("\n- ")
+end
+
 def format_article(article)
   "#{article[:title]}\n" +
   "by: #{article[:author]}\n" +
@@ -81,14 +109,12 @@ def format_footer(campus)
   "Work hard, be nice, and have an epic week! //\n Flatiron School · #{campus[:name]} · #{campus[:address]} "
 end
 
-
 #########################
 # entry point - generates the actual newsletter
 #########################
 
 def run
-  puts "Generating this week's newsletter..."
-  puts ''
+  puts "Generating this week's newsletter...\n"
   print 'Subject: '
   print_subject
   print 'Recipients: '
